ca0449130f12d95b3260361980bd54496366a78b
Sujay08 <sujaysrinivasan08@gmail.com>
2020-11-24 08:51:49 +0530

    route change

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,8 @@ const express = require('express');
 const User = require("../models/user");
 const bcrypt = require('bcrypt');
 const router = express.Router();
-
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -25,5 +26,36 @@ router.post('/signup', (req, res, next) => {
                 })
         });
 });
+
+router.post("/login", (res,req,next)=>{
+    User.findOne({email: req.body.email}).then(user=>{
+        if(!user){
+            return res.status(401).json({
+                message: "Auth failed"
+            });
+        }
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result=>{
+        if(!result){
+            return res.status(401).json({
+                message: "Auth failed"
+            })
+        }
+        const token = jwt.sign({
+            email: user.email, userId: user._id
+        }, 
+        "YOO_DAWG_THIS_IS_MY_PRIVATE_KEY",
+        {
+            expiresIn: "1h"
+        },
+        );
+    })
+    .catch(err=>{
+        return res.status(401).json({
+            message: "Auth failed"
+        })
+    })
+})
 
 module.exports = router
